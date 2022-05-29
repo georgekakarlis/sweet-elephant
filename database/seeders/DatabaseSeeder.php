b7ed70652ca4c23e977757065a48a76842085d5b
George Kakarlis <georgekakarlis@yahoo.com>
2022-05-29 13:00:20 +0200

    more sunday-PHP-funday

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(5)->create();
 
-       Listing::create([
+         Listing::factory(6)->create();
+
+      /*  Listing::create([
         'title' => 'Laravel Senior Developer', 
         'tags' => 'laravel, javascript',
         'company' => 'Acme Corp',
@@ -34,6 +36,6 @@ class DatabaseSeeder extends Seeder
        'email' => 'email2@email.com',
        'website' => 'https://www.starkindustries.com',
        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
-     ]);
+     ]); */
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,11 @@ Route::get('/', function () {
 });
 
 
-//SINGLE LISTING
-Route::get('/listings/{id}', function($id) {
+//SINGLE LISTING      ROUTE MODEL BINDING
+Route::get('/listings/{listing}', function(Listing $listing) {
     return view('listing', [
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
+    
+
 });
